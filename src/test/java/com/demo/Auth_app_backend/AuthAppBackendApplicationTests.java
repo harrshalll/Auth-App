a14eb30e7ae3651f828dad39df788ee2df7c4d5c
harrshalll <havhad143@gmail.com>
2026-03-15 17:30:48 +0530

    Initial Commit

---
+++ src/test/java/com/demo/Auth_app_backend/AuthAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Auth_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
